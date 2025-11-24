792274692fba3d4af644e2119bf7b76cd73476ac
sabihaaya2006-max <sabihaaya2006@gmail.com>
2025-11-23 20:47:44 -0500

    third commit

--- python_github_assignment/app.py
+++ python_github_assignment/app.py
@@ -4,4 +4,9 @@ print("Welcome to my Python program!")
 # Ask for user input
 hours = input("How many hours did you study today?")
 
+try:
+     # Convert input to a number
+    hours = float(hours)
 
+    # Perform a calculation
+    weekly_hours = hours * 7
